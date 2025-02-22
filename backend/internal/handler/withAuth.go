4cde428fcf4579a6a4d6b71c7015ce07b510b868
tomek7667 <git@cyber-man.pl>
2025-02-23 00:47:32 +0100

    withAuth return consistent

--- backend/internal/handler/withAuth.go
+++ backend/internal/handler/withAuth.go
@@ -8,16 +8,20 @@ func (c *Client) WithAuth() func(ctx *gin.Context) {
 	return func(ctx *gin.Context) {
 		token := ctx.GetHeader("Authorization")
 		if token == "" {
-			ctx.JSON(401, map[string]string{
-				"error": "missing authorization header",
+			ctx.JSON(401, map[string]interface{}{
+				"success": false,
+				"message": "missing authorization header",
+				"data":    nil,
 			})
 			ctx.Abort()
 			return
 		}
 		user, err := c.UserService.Verify(token)
 		if err != nil {
-			ctx.JSON(401, map[string]string{
-				"error": err.Error(),
+			ctx.JSON(401, map[string]interface{}{
+				"success": false,
+				"message": err.Error(),
+				"data":    nil,
 			})
 			ctx.Abort()
 			return
